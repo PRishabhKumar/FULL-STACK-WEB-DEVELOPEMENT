cb857e5c783cd46e81b9096c8807ef5a9503ec20
P Rishabh Kumar <rishabh260405@gmail.com>
2025-01-15 10:34:35 +0530

    Advanced JavaScript

---
+++ LEARNING JAVASCRIPT/Advanced JavaScript/app.js
@@ -0,0 +1,175 @@
+let heading = document.querySelector("#ayncJS");
+
+function changeColour(colour, delay, nextColour) {
+  setTimeout(() => {
+    heading.style.color = colour;
+    if (nextColour) {
+      nextColour();
+    }
+  }, delay);
+}
+
+changeColour("orange", 500, () => {
+  changeColour("red", 500, () => {
+    changeColour("green", 500, () => {
+      changeColour("blue", 500, () => {
+        changeColour("yellow", 500, () => {
+          changeColour("pink", 500, () => {
+            changeColour("purple", 500, () => {
+              changeColour("royalblue", 500, () => {
+                changeColour("violet", 500, () => {
+                  changeColour("indigo", 500);
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+// Below is one of the methods on how to achieve a simple system that stores a data in the database based on the internet speed available.
+function saveData(data) {
+  let internetSpeed = Math.floor(Math.random() * 10) + 1; // generating random numbers between 1 and 10 as ther internet speed
+  if (internetSpeed >= 5) {
+    alert("Data was saved successfully !!");
+  } else {
+    alert("Data was not saved in the database due to low internet speed !!!");
+  }
+}
+
+// saveData("P Rishabh Kumar");
+
+// In the above approach , we are only prinitng a single line both in the case of success as well as failure but if we want to execute a lot of other lines of code upon success and failure, then we need to create functions for them and pass those functions as parameters to the main function as shown below
+
+function newSaveData(data, success, failure) {
+  let internetSpeed = Math.floor(Math.random() * 10) + 1;
+  if (internetSpeed >= 5) {
+    success();
+  } else {
+    failure();
+  }
+}
+
+// this is another representation where what to do in the case of failure and success is not explicitly mentioned but is stored in dedidcated functions
+
+// newSaveData(
+//   "P Rishabh Kumar",
+//   () => {
+//     console.log("Your data was saved successfully !!!");
+//     newSaveData(
+//       "Yug Raithatha",
+//       () => {
+//         console.log("Success2: The second data was saved successfully");
+//         newSaveData(
+//           "Rajvi Solanki",
+//           () => {
+//             console.log("Success3: The third data was saved successfully");
+//           },
+//           () => {
+//             console.log(
+//               "Failure3: The thir data could not be saved due to slow internet speeds"
+//             );
+//           }
+//         );
+//       },
+//       () => {
+//         console.log(
+//           "Failure2: The second data could not be saved due to low internet speeds"
+//         );
+//       }
+//     );
+//   },
+//   () => {
+//     console.log("Data not saved due to slow internet speeds");
+//   }
+// );
+
+// promise is an object in JS which assures you that some operation will be performed although it is uncertain whether it will be successfull or not.
+
+// This object has three states and two callbacks.
+
+// The states are
+// (i) Pending which means it is not sure whether the operation will be successfull or not as it is not yet processed.
+// (ii) Fulfilled which means whatever task we had to perform has been performed successfully
+// (ii) Rejected which means the desired operation (here saving a data in the data base) was unseccessful
+
+// The callbacks it takes are :
+// (i) resolve() which is executed when the operation is completed successfully
+// (ii) raject() which is executed when the operation fails
+
+// Now we will see how we can write the above confusing code in a comparatively simpler format using promises
+
+function saveToDBUsingPromise(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed >= 5) {
+      resolve("Data saved");
+    } else {
+      reject("Data not saved due to slow internet speeds");
+    }
+  });
+}
+
+//this is the approach we use using promises
+// Now lets see how to refactor the above code using promises
+
+// Now we will see about the then() and catch() function as they are used to write the code that has be executed when the operation succeeds and fails respectively and we will use these methods to get the structure of the above long confusing code using promises
+
+// let reponse = saveToDBUsingPromise("P S Rao");
+// console.log(reponse);
+// reponse
+//   .then(() => {
+//     console.log("Success: Data was saved successfully");
+//     let reponse1 = saveToDBUsingPromise("P Laxmi");
+//     response1
+//       .then(() => {
+//         console.log("Success2: The second data was saced successfully");
+//         let response2 = saveToDBUsingPromise("P Rishabh Kumar")
+//           .then(() => {
+//             console.log("Success3: The third data was saved successfully");
+//           })
+//           .catch(() => {
+//             console.log(
+//               "Failure3: The third data was not saved successfully due to slow internet speeds"
+//             );
+//           });
+//       })
+//       .catch(() => {
+//         console.log(
+//           "Failure2: The second data was not saved due to slow internet speeds"
+//         );
+//       });
+//   })
+//   .catch(() => {
+//     console.log("Data was not saved due to slow internet speeds !!!");
+//   });
+
+// a further compact format to represent the above code is as shown below :
+
+saveToDBUsingPromise("P S Rao")
+  .then(() => {
+    console.log("Success: Data was saved successfully");
+    saveToDBUsingPromise("P Laxmi")
+      .then(() => {
+        console.log("Success2: The second data was saced successfully");
+        saveToDBUsingPromise("P Rishabh Kumar")
+          .then(() => {
+            console.log("Success3: The third data was saved successfully");
+          })
+          .catch(() => {
+            console.log(
+              "Failure3: The third data was not saved successfully due to slow internet speeds"
+            );
+          });
+      })
+      .catch(() => {
+        console.log(
+          "Failure2: The second data was not saved due to slow internet speeds"
+        );
+      });
+  })
+  .catch(() => {
+    console.log("Data was not saved due to slow internet speeds !!!");
+  });
