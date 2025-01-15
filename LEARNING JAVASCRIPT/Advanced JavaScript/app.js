d5573f8d4b760b6422bf2dd9fc897c3d8cdabf1f
P Rishabh Kumar <rishabh260405@gmail.com>
2025-01-15 11:02:29 +0530

    Changes

--- LEARNING JAVASCRIPT/Advanced JavaScript/app.js
+++ LEARNING JAVASCRIPT/Advanced JavaScript/app.js
@@ -173,3 +173,70 @@ saveToDBUsingPromise("P S Rao")
   .catch(() => {
     console.log("Data was not saved due to slow internet speeds !!!");
   });
+
+// Now lets try refactor the code we wrote to change the colour of the heading periodically using async functions and the await keyword
+
+let newHeading = document.querySelector(".asyncAwait");
+// function newColourChange(color, delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       newHeading.style.color = color;
+//       console.log(`The colour of the heding has been change to ${color}`);
+//       resolve("Colour change successful !!");
+//     }, delay);
+//   });
+// }
+
+// async function colourChangeDemo() {
+//   await newColourChange("orange", 1000);
+//   await newColourChange("red", 1000);
+//   await newColourChange("green", 1000);
+//   await newColourChange("blue", 1000);
+//   await newColourChange("yellow", 1000);
+//   await newColourChange("pink", 1000);
+//   await newColourChange("purple", 1000);
+//   await newColourChange("royalblue", 1000);
+//   await newColourChange("violet", 1000);
+//   await newColourChange("indigo", 1000);
+// }
+
+// While using multiple function calls using async-await keywords, there is a possibility that out of say the 10 function calls we are makiing any one random function call may result in a rejected promise and in this case the console will throw an error. If we want to perform some more operations after the function calls are over then these instruction will never be processed due to error thrown by the rejected promise.
+// To resolve this issue, we can handle the rejections by keeping all those function calls which can throw an error inside the try block and catching the errors in the catch block as shown below
+
+function newColourChange(color, delay) {
+  return new Promise((resolve, reject) => {
+    let number = Math.floor(Math.random() * 10) + 1;
+    if (number >= 5) {
+      setTimeout(() => {
+        newHeading.style.color = color;
+        console.log(`Color changed to ${color}`);
+        resolve("Color changed successfully !!");
+      }, delay);
+    } else {
+      reject(
+        `The colour coould not be changed to ${color} due to some error !!!`
+      );
+    }
+  });
+}
+
+async function changeColourDemo() {
+  try {
+    await newColourChange("orange", 1000);
+    await newColourChange("red", 1000);
+    await newColourChange("green", 1000);
+    await newColourChange("blue", 1000);
+    await newColourChange("yellow", 1000);
+    await newColourChange("pink", 1000);
+    await newColourChange("purple", 1000);
+    await newColourChange("royalblue", 1000);
+    await newColourChange("violet", 1000);
+    await newColourChange("indigo", 1000);
+  } catch (error) {
+    console.log("Some error occured");
+    console.log(error);
+  }
+  console.log(
+    "This operation is intended to be performed after the execution of the function calls !!!"
+  );
+}
